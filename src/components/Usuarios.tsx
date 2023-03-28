bc9df93c01a815bd0643416fefdb28dc91cbd0f2
YhonattanZ <yonattanz11@gmail.com>
2023-03-27 21:40:09 -0400

    add Mostrar usuarios en la pantalla

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,21 +1,33 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 import { reqResAPI } from '../api/reqRes';
-import { ReqResList } from '../interfaces/reqRes';
+import { ReqResList, Usuario } from '../interfaces/reqRes';
 
 export const Usuarios = () => {
 
-const [usuarios, setUsuarios] = useState([]);  
+const [usuarios, setUsuarios] = useState<Usuario[]>([
+
+]);  
     
 useEffect(() => {
   //API call
   reqResAPI.get<ReqResList>('/users').then(resp => {
-    console.log(resp.data.data);
+    
+    setUsuarios(resp.data.data);
   })
   .catch(console.log);  
   
 }, [])
 
+const renderInfo = ({id,first_name,last_name,email,avatar}: Usuario) =>{
+    return (
+        <tr key={id}>
+           <td> <img src={avatar} alt={first_name} style={{width:35,borderRadius:100}}/></td>
+            <td>{first_name} {last_name}</td>
+            <td>{email}</td>
+        </tr>
+    )
+}
 
   return (
  <>
@@ -29,9 +41,15 @@ useEffect(() => {
 </tr>
     </thead>
     <tbody>
-
+        {
+        //   usuarios.map(usuario => renderInfo(usuario))  
+        usuarios.map(renderInfo)
+        }
     </tbody>
  </table>
+ <button className='btn btn-primary'>
+    Siguiente
+ </button>
  </>
   )
 }
